30c89f170ea2a598aede42a90db99dac67832ecf
sakkke <w32w64@gmail.com>
2022-11-13 11:04:14 +0000

    feat: implement basic functions before inside
    Closes #33.

--- config.v
+++ config.v
@@ -1,6 +1,8 @@
 module main
 
 const default_config_map = {
-	'console_keymap': ['us']
-	'disk':           ['/dev/sda']
+	'console_keymap':    ['us']
+	'disk':              ['/dev/sda']
+	'installation_mode': ['Custom']
+	'packages':          []
 }
