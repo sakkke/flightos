589210010311635da40c09ef93f4662e126393c8
sakkke <w32w64@gmail.com>
2022-11-23 06:32:51 +0000

    fix: fix setup.sh [skip ci]
    Closes #63.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -eu
-temp_bin="$(mktemp -d)"
+temp="$(mktemp -d)"
+temp_bin="$temp/bin"
 mkdir "$temp_bin"
 trap "rm -fr \"$temp\"" EXIT
 ver="$(curl -s https://raw.githubusercontent.com/sakkke/flightos/main/version.txt)"
 FLIGHTOS="$temp/flightos"
-FLIGHTOS_URL="https://github.com/sakkke/flightos/releases/download/$(ver)/flightos-$(ver).tar.gz"
-FZF_URL=https://github.com/junegunn/fzf/releases/download/0.35.0/fzf-0.35.0-linux_arm64.tar.gz
+FLIGHTOS_URL="https://github.com/sakkke/flightos/releases/download/$ver/flightos-$ver.tar.gz"
+FZF_URL=https://github.com/junegunn/fzf/releases/download/0.35.0/fzf-0.35.0-linux_amd64.tar.gz
 curl -Ls "$FLIGHTOS_URL" | tar -zxf - -C "$temp_bin"
-if ! type fzf 2>&1 > /dev/null; then
+if ! type fzf > /dev/null 2>&1; then
     curl -Ls "$FZF_URL" | tar -zxf - -C "$temp_bin"
 fi
 PATH="$temp_bin:$PATH" flightos "$@"
