d43d52cdba09deea0c041ab2e31120351d183556
sakkke <w32w64@gmail.com>
2022-11-23 02:10:37 +0000

    feat: add version.txt [skip ci]
    Closes #50.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ set -eu
 temp_bin="$(mktemp -d)"
 mkdir "$temp_bin"
 trap "rm -fr \"$temp\"" EXIT
+ver="$(curl -s https://raw.githubusercontent.com/sakkke/flightos/main/version.txt)"
 FLIGHTOS="$temp/flightos"
-FLIGHTOS_URL=https://github.com/sakkke/flightos/releases/download/2022.11.4/flightos-2022.11.4.tar.gz
+FLIGHTOS_URL="https://github.com/sakkke/flightos/releases/download/$(ver)/flightos-$(ver).tar.gz"
 FZF_URL=https://github.com/junegunn/fzf/releases/download/0.35.0/fzf-0.35.0-linux_arm64.tar.gz
 curl -Ls "$FLIGHTOS_URL" | tar -zxf - -C "$temp_bin"
 if ! type fzf 2>&1 > /dev/null; then
