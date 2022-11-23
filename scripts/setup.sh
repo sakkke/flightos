427b570fb6d7dfeb3365b8d390d3d362d2534684
sakkke <w32w64@gmail.com>
2022-11-23 06:39:44 +0000

    chore: bump fzf [skip ci]
    Closes #65.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ trap "rm -fr \"$temp\"" EXIT
 ver="$(curl -s https://raw.githubusercontent.com/sakkke/flightos/main/version.txt)"
 FLIGHTOS="$temp/flightos"
 FLIGHTOS_URL="https://github.com/sakkke/flightos/releases/download/$ver/flightos-$ver.tar.gz"
-FZF_URL=https://github.com/junegunn/fzf/releases/download/0.35.0/fzf-0.35.0-linux_amd64.tar.gz
+FZF_URL=https://github.com/junegunn/fzf/releases/download/0.35.1/fzf-0.35.1-linux_amd64.tar.gz
 curl -Ls "$FLIGHTOS_URL" | tar -zxf - -C "$temp_bin"
 if ! type fzf > /dev/null 2>&1; then
     curl -Ls "$FZF_URL" | tar -zxf - -C "$temp_bin"
